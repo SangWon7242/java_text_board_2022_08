254471c862c1e7df5f8f7e0c3df95c624f804734
SangWon7242 <gotkdqja@gmail.com>
2022-08-10 14:40:43 +0900

    등록시마다 게시물 번호가 증가하도록

--- src/com/psw/exam/board/Main.java
+++ src/com/psw/exam/board/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+    int articleLastId = 0;
 
     System.out.println("== 게시판 v 0.1 ==");
     System.out.println("== 프로그램 시작 ==");
@@ -22,7 +23,8 @@ public class Main {
         String title = sc.nextLine();
         System.out.printf("내용 : ");
         String body = sc.nextLine();
-        int id = 1;
+        int id = articleLastId + 1;
+        articleLastId = id;
         System.out.printf("%d번 게시물이 입력 되었습니다.\n", id);
       }
       else {
