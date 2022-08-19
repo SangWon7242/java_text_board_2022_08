ff28ddf75b6deb207564956f1930e8810d967ecc
SangWon7242 <gotkdqja@gmail.com>
2022-08-19 15:36:06 +0900

    actionUsrArticleDetail 메서드 구현

--- src/com/psw/exam/board/Main.java
+++ src/com/psw/exam/board/Main.java
@@ -54,33 +54,7 @@ public class Main {
         System.out.printf("%d번 게시물이 입력 되었습니다.\n", article.id);
       }
       else if (rq.getUrlPath().equals("/usr/article/detail")) {
-
-        if( params.containsKey("id") == false ) {
-          System.out.println("id를 입력해주세요.");
-          continue;
-        }
-
-        int id = 0;
-
-        try {
-          id = Integer.parseInt(params.get("id"));
-        }
-        catch (NumberFormatException e) {
-          System.out.println("id를 정수 형태롤 입력해주세요.");
-          continue;
-        }
-
-        Article article = articles.get(id - 1);
-
-        if( id > articles.size() ) {
-          System.out.println("게시물이 존재하지 않습니다.");
-          continue;
-        }
-
-        System.out.println("== 게시물 상세내용 ==");
-        System.out.printf("번호 : %d\n", article.id);
-        System.out.printf("제목 : %s\n", article.title);
-        System.out.printf("내용 : %s\n", article.body);
+        actionUsrArticleDetail(rq, articles);
       }
 
       else {
@@ -90,6 +64,37 @@ public class Main {
     System.out.println("== 프로그램 종료 ==");
 
     sc.close();
+  }
+
+  private static void actionUsrArticleDetail(Rq rq, List<Article> articles) {
+    Map<String, String> params = rq.getParams();
+
+    if( params.containsKey("id") == false ) {
+      System.out.println("id를 입력해주세요.");
+      return;
+    }
+
+    int id = 0;
+
+    try {
+      id = Integer.parseInt(params.get("id"));
+    }
+    catch (NumberFormatException e) {
+      System.out.println("id를 정수 형태롤 입력해주세요.");
+      return;
+    }
+
+    Article article = articles.get(id - 1);
+
+    if( id > articles.size() ) {
+      System.out.println("게시물이 존재하지 않습니다.");
+      return;
+    }
+
+    System.out.println("== 게시물 상세내용 ==");
+    System.out.printf("번호 : %d\n", article.id);
+    System.out.printf("제목 : %s\n", article.title);
+    System.out.printf("내용 : %s\n", article.body);
   }
 
   private static void actionUsrArticleList(Rq rq, List<Article> articles) {
