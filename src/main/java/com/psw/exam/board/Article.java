573b325aaad157f532048ca8b80ab1ac321b3d48
SangWon7242 <gotkdqja@gmail.com>
2022-08-25 14:30:21 +0900

    롬복 적용

--- src/main/java/com/psw/exam/board/Article.java
+++ src/main/java/com/psw/exam/board/Article.java
@@ -1,19 +1,17 @@
 package com.psw.exam.board;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Article {
   int id;
   String title;
   String body;
 
-  Article(int id, String title, String body) {
-    this.id = id;
-    this.title = title;
-    this.body = body;
-  }
 
-  @Override
-  public String toString() {
-    return String.format("{id : %d, title : \"%s\", body : \"%s\"}", id, title, body);
-  }
 }
 
